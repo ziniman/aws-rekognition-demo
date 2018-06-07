47b9c76b1b3258af17c82eb151480fe343c6ed60
Boaz Ziniman <boazz@amazon.com>
2018-06-07 14:05:44 +0300

    initial code

---
+++ compare.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python2.7
+
+import boto3
+import os
+import json
+import argparse
+
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+# command line arguments
+parser = argparse.ArgumentParser(description='Compare faces in 2 image file with Rekognition')
+parser.add_argument('source', help='Souece image name')
+parser.add_argument('target', help='Target image name')
+parser.add_argument('region', default='us-east-1', nargs='?', help='Comprehend region (default=us-east-1')
+args = parser.parse_args()
+print(args)
+
+if __name__ == "__main__":
+
+    sourceFile=args.source
+    targetFile=args.target
+
+    os.system('open -a Preview %s' % sourceFile)
+    os.system('open -a Preview %s' % targetFile)
+    client=boto3.client('rekognition')
+
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+
+    response=client.compare_faces(SimilarityThreshold=50,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+
+    #print json.dumps(response, sort_keys=True, indent=4)
+
+    if not response['FaceMatches']:
+        print bcolors.RED + 'No Match'
+    else:
+        for faceMatch in response['FaceMatches']:
+            position = faceMatch['Face']['BoundingBox']
+            confidence = str(faceMatch['Face']['Confidence'])
+            print(bcolors.GREEN + 'The face at ' +
+                   str(position['Left']) + ' ' +
+                   str(position['Top']) +
+                   ' matches with ' + confidence + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()
---
+++ match.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python2.7
+
+import boto3
+import os
+import json
+import argparse
+
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+# command line arguments
+parser = argparse.ArgumentParser(description='Match face from image file with Rekognition collection')
+parser.add_argument('image', help='Souece image name')
+parser.add_argument('collection', help='Collection name')
+parser.add_argument('region', default='us-east-1', nargs='?', help='Comprehend region (default=us-east-1')
+args = parser.parse_args()
+print(args)
+
+if __name__ == "__main__":
+
+    bucket='bucket'
+    collectionId=args.collection
+    fileName=args.image
+    threshold = 70
+    maxFaces=2
+
+    client=boto3.client('rekognition')
+    os.system('open -a Preview %s' % fileName)
+
+    imageSource=open(fileName,'rb')
+
+    response=client.search_faces_by_image(CollectionId=collectionId,
+                                Image={'Bytes': imageSource.read()},
+                                FaceMatchThreshold=threshold,
+                                MaxFaces=maxFaces)
+
+    #print json.dumps(response, sort_keys=True, indent=4)
+
+    faceMatches=response['FaceMatches']
+    if not faceMatches:
+        print bcolors.RED + 'No Match!'
+    else:
+        print ('Matching faces')
+        for match in faceMatches:
+                print (bcolors.BLUE + 'FaceId:' + match['Face']['FaceId'])
+                print (bcolors.BLUE + 'FaceId:' + match['Face']['ExternalImageId'])
+                print (bcolors.GREEN + 'Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
